236410a2ffb14109cb5438ca71d93a006052cbec
Manishmsn <mk065005@gmail.com>
2026-04-09 16:02:51 +0530

    avoid display name if its null

--- storeprocedure/api_ccfl_ma_get_user.sql
+++ storeprocedure/api_ccfl_ma_get_user.sql
@@ -1,177 +1,177 @@
-  
-  
-  
--- ===============================================================================================================  
--- Author:   Tiago Medeiros  
--- Last Updated: 11.29.2022  
--- Description:  Used to Get User  
--- Application:  CCFTL API  
--- Updates:   
--- 08/10/2024 MCG Remove dp_Users.User Email references as this field is being removed by ACST.  
--- 09/01/2026 Bounteous added Nickname to the response  
--- ===============================================================================================================  
-  
-CREATE PROCEDURE [dbo].[api_ccfl_ma_get_user]  
-    @UserID AS INT  
-  , @UserName AS VARCHAR(80) = NULL  
-  , @Email AS VARCHAR(80) = NULL  
-  , @Phone AS VARCHAR(80) = NULL  
-AS  
-BEGIN  
-    IF @UserID > 0  
-        SELECT u.[User_ID]  
-             , u.[User_Name]  
-             , COALESCE(u.[Display_Name], '') AS Display_Name  
-             , u.[Password]  
-             , u.[Admin]  
-             , u.[Domain_ID]  
-             , u.[Publications_Manager]  
-             , u.[Contact_ID]  
-             , u.[Supervisor]  
-             , u.[User_GUID]  
-             , u.[Can_Impersonate]  
-             , u.[__ShelbyID]  
-             , u.[__Old_User_ID]  
-             , u.[In_Recovery]  
-             , u.[Data_Service_Permissions]  
-             , u.[Read_Permitted]  
-             , u.[Create_Permitted]  
-             , u.[Update_Permitted]  
-             , u.[Delete_Permitted]  
-             , u.[Time_Zone]  
-             , u.[Locale]  
-             , u.[Theme]  
-             , u.[Setup_Admin]  
-             , u.[Local_Network_Name]  
-             , u.[Login_Attempts]  
-             , u.[MFA_Required]  
-             , u.[Delete_Account_Info]  
-             , u.[Delete_Requested_Date]  
-             , u.[Reason_Deleted]  
-             , u.[__Unmerge_Date]  
-             , COALESCE(c.First_Name, '') AS First_Name  
-             , COALESCE(c.Last_Name, '') AS Last_Name  
-             , COALESCE(c.Nickname, '') AS Nickname  
-        FROM dp_Users u  
-             LEFT OUTER JOIN Contacts c ON c.User_Account = u.User_ID  
-        WHERE User_ID = @UserID;  
-    ELSE IF @UserName IS NOT NULL  
-        SELECT u.[User_ID]  
-             , u.[User_Name]  
-             , COALESCE(u.[Display_Name], '') AS Display_Name  
-             , u.[Password]  
-             , u.[Admin]  
-             , u.[Domain_ID]  
-             , u.[Publications_Manager]  
-             , u.[Contact_ID]  
-             , u.[Supervisor]  
-             , u.[User_GUID]  
-             , u.[Can_Impersonate]  
-             , u.[__ShelbyID]  
-             , u.[__Old_User_ID]  
-             , u.[In_Recovery]  
-             , u.[Data_Service_Permissions]  
-             , u.[Read_Permitted]  
-             , u.[Create_Permitted]  
-             , u.[Update_Permitted]  
-             , u.[Delete_Permitted]  
-             , u.[Time_Zone]  
-             , u.[Locale]  
-             , u.[Theme]  
-             , u.[Setup_Admin]  
-             , u.[Local_Network_Name]  
-             , u.[Login_Attempts]  
-             , u.[MFA_Required]  
-             , u.[Delete_Account_Info]  
-             , u.[Delete_Requested_Date]  
-             , u.[Reason_Deleted]  
-             , u.[__Unmerge_Date]  
-             , COALESCE(c.First_Name, '') AS First_Name  
-             , COALESCE(c.Last_Name, '') AS Last_Name  
-             , COALESCE(c.Nickname, '') AS Nickname  
-        FROM dp_Users u  
-             LEFT OUTER JOIN Contacts c ON c.User_Account = u.User_ID  
-        WHERE User_Name LIKE @UserName;  
-    ELSE IF @Email IS NOT NULL  
-        SELECT u.[User_ID]  
-             , u.[User_Name]  
-             , COALESCE(u.[Display_Name], '') AS Display_Name  
-             , u.[Password]  
-             , u.[Admin]  
-             , u.[Domain_ID]  
-             , u.[Publications_Manager]  
-             , u.[Contact_ID]  
-             , u.[Supervisor]  
-             , u.[User_GUID]  
-             , u.[Can_Impersonate]  
-             , u.[__ShelbyID]  
-             , u.[__Old_User_ID]  
-             , u.[In_Recovery]  
-             , u.[Data_Service_Permissions]  
-             , u.[Read_Permitted]  
-             , u.[Create_Permitted]  
-             , u.[Update_Permitted]  
-             , u.[Delete_Permitted]  
-             , u.[Time_Zone]  
-             , u.[Locale]  
-             , u.[Theme]  
-             , u.[Setup_Admin]  
-        , u.[Local_Network_Name]  
-             , u.[Login_Attempts]  
-             , u.[MFA_Required]  
-             , u.[Delete_Account_Info]  
-             , u.[Delete_Requested_Date]  
-             , u.[Reason_Deleted]  
-             , u.[__Unmerge_Date]  
-             , COALESCE(c.First_Name, '') AS First_Name  
-             , COALESCE(c.Last_Name, '') AS Last_Name  
-             , COALESCE(c.Nickname, '') AS Nickname  
-        FROM dp_Users u  
-             LEFT OUTER JOIN Contacts c ON c.User_Account = u.User_ID  
-        WHERE User_Name LIKE @Email  
-              OR c.Email_Address LIKE @Email;  
-    ELSE  
-        SELECT u.[User_ID]  
-             , u.[User_Name]  
-             , COALESCE(u.[Display_Name], '') AS Display_Name  
-             , u.[Password]  
-             , u.[Admin]  
-             , u.[Domain_ID]  
-             , u.[Publications_Manager]  
-             , u.[Contact_ID]  
-             , u.[Supervisor]  
-             , u.[User_GUID]  
-             , u.[Can_Impersonate]  
-             , u.[__ShelbyID]  
-             , u.[__Old_User_ID]  
-             , u.[In_Recovery]  
-             , u.[Data_Service_Permissions]  
-             , u.[Read_Permitted]  
-             , u.[Create_Permitted]  
-             , u.[Update_Permitted]  
-             , u.[Delete_Permitted]  
-             , u.[Time_Zone]  
-             , u.[Locale]  
-             , u.[Theme]  
-             , u.[Setup_Admin]  
-             , u.[Local_Network_Name]  
-             , u.[Login_Attempts]  
-             , u.[MFA_Required]  
-             , u.[Delete_Account_Info]  
-             , u.[Delete_Requested_Date]  
-             , u.[Reason_Deleted]  
-             , u.[__Unmerge_Date]  
-             , COALESCE(c.First_Name, '') AS First_Name  
-             , COALESCE(c.Last_Name, '') AS Last_Name  
-             , COALESCE(c.Nickname, '') AS Nickname  
-        FROM dp_Users u  
-             INNER JOIN Contacts c ON c.Contact_ID = u.Contact_ID  
-        WHERE REPLACE (REPLACE (REPLACE (c.Mobile_Phone, '-', ''), ')', ''), '(', '') LIKE REPLACE (  
-                                                                                               REPLACE (  
-                                                                                                   REPLACE (  
-                                                                                                       @Phone, '-', '')  
-                                                                                                 , ')'  
-                                                                                                 , '')  
-                                                                                             , '('  
-                                                                                             , '');  
-END;  
+    
+    
+    
+-- ===============================================================================================================    
+-- Author:   Tiago Medeiros    
+-- Last Updated: 11.29.2022    
+-- Description:  Used to Get User    
+-- Application:  CCFTL API    
+-- Updates:     
+-- 08/10/2024 MCG Remove dp_Users.User Email references as this field is being removed by ACST.    
+-- 09/01/2026 Bounteous added Nickname to the response    
+-- ===============================================================================================================    
+    
+alter PROCEDURE [dbo].[api_ccfl_ma_get_user]    
+    @UserID AS INT    
+  , @UserName AS VARCHAR(80) = NULL    
+  , @Email AS VARCHAR(80) = NULL    
+  , @Phone AS VARCHAR(80) = NULL    
+AS    
+BEGIN    
+    IF @UserID > 0    
+        SELECT u.[User_ID]    
+             , u.[User_Name]    
+             , COALESCE(NULLIF(LTRIM(RTRIM(u.[Display_Name])), ''), NULLIF(LTRIM(RTRIM(CONCAT(COALESCE(c.First_Name, ''), ' ', COALESCE(c.Last_Name, '')))), ''), NULLIF(LTRIM(RTRIM(c.Nickname)), ''), u.[User_Name], '') AS Display_Name    
+             , u.[Password]    
+             , u.[Admin]    
+             , u.[Domain_ID]    
+             , u.[Publications_Manager]    
+             , u.[Contact_ID]    
+             , u.[Supervisor]    
+             , u.[User_GUID]    
+             , u.[Can_Impersonate]    
+             , u.[__ShelbyID]    
+             , u.[__Old_User_ID]    
+             , u.[In_Recovery]    
+             , u.[Data_Service_Permissions]    
+             , u.[Read_Permitted]    
+             , u.[Create_Permitted]    
+             , u.[Update_Permitted]    
+             , u.[Delete_Permitted]    
+             , u.[Time_Zone]    
+             , u.[Locale]    
+             , u.[Theme]    
+             , u.[Setup_Admin]    
+             , u.[Local_Network_Name]    
+             , u.[Login_Attempts]    
+             , u.[MFA_Required]    
+             , u.[Delete_Account_Info]    
+             , u.[Delete_Requested_Date]    
+             , u.[Reason_Deleted]    
+             , u.[__Unmerge_Date]    
+             , COALESCE(c.First_Name, '') AS First_Name    
+             , COALESCE(c.Last_Name, '') AS Last_Name    
+             , COALESCE(c.Nickname, '') AS Nickname    
+        FROM dp_Users u    
+             LEFT OUTER JOIN Contacts c ON c.User_Account = u.User_ID    
+        WHERE User_ID = @UserID;    
+    ELSE IF @UserName IS NOT NULL    
+        SELECT u.[User_ID]    
+             , u.[User_Name]    
+             , COALESCE(NULLIF(LTRIM(RTRIM(u.[Display_Name])), ''), NULLIF(LTRIM(RTRIM(CONCAT(COALESCE(c.First_Name, ''), ' ', COALESCE(c.Last_Name, '')))), ''), NULLIF(LTRIM(RTRIM(c.Nickname)), ''), u.[User_Name], '') AS Display_Name    
+             , u.[Password]    
+             , u.[Admin]    
+             , u.[Domain_ID]    
+             , u.[Publications_Manager]    
+             , u.[Contact_ID]    
+             , u.[Supervisor]    
+             , u.[User_GUID]    
+             , u.[Can_Impersonate]    
+             , u.[__ShelbyID]    
+             , u.[__Old_User_ID]    
+             , u.[In_Recovery]    
+             , u.[Data_Service_Permissions]    
+             , u.[Read_Permitted]    
+             , u.[Create_Permitted]    
+             , u.[Update_Permitted]    
+             , u.[Delete_Permitted]    
+             , u.[Time_Zone]    
+             , u.[Locale]    
+             , u.[Theme]    
+             , u.[Setup_Admin]    
+             , u.[Local_Network_Name]    
+             , u.[Login_Attempts]    
+             , u.[MFA_Required]    
+             , u.[Delete_Account_Info]    
+             , u.[Delete_Requested_Date]    
+             , u.[Reason_Deleted]    
+             , u.[__Unmerge_Date]    
+             , COALESCE(c.First_Name, '') AS First_Name    
+             , COALESCE(c.Last_Name, '') AS Last_Name    
+             , COALESCE(c.Nickname, '') AS Nickname    
+        FROM dp_Users u    
+             LEFT OUTER JOIN Contacts c ON c.User_Account = u.User_ID    
+        WHERE User_Name LIKE @UserName;    
+    ELSE IF @Email IS NOT NULL    
+        SELECT u.[User_ID]    
+             , u.[User_Name]    
+             , COALESCE(NULLIF(LTRIM(RTRIM(u.[Display_Name])), ''), NULLIF(LTRIM(RTRIM(CONCAT(COALESCE(c.First_Name, ''), ' ', COALESCE(c.Last_Name, '')))), ''), NULLIF(LTRIM(RTRIM(c.Nickname)), ''), u.[User_Name], '') AS Display_Name    
+             , u.[Password]    
+             , u.[Admin]    
+             , u.[Domain_ID]    
+             , u.[Publications_Manager]    
+             , u.[Contact_ID]    
+             , u.[Supervisor]    
+             , u.[User_GUID]    
+             , u.[Can_Impersonate]    
+             , u.[__ShelbyID]    
+             , u.[__Old_User_ID]    
+             , u.[In_Recovery]    
+             , u.[Data_Service_Permissions]    
+             , u.[Read_Permitted]    
+             , u.[Create_Permitted]    
+             , u.[Update_Permitted]    
+             , u.[Delete_Permitted]    
+             , u.[Time_Zone]    
+             , u.[Locale]    
+             , u.[Theme]    
+             , u.[Setup_Admin]    
+        , u.[Local_Network_Name]    
+             , u.[Login_Attempts]    
+             , u.[MFA_Required]    
+             , u.[Delete_Account_Info]    
+             , u.[Delete_Requested_Date]    
+             , u.[Reason_Deleted]    
+             , u.[__Unmerge_Date]    
+             , COALESCE(c.First_Name, '') AS First_Name    
+             , COALESCE(c.Last_Name, '') AS Last_Name    
+             , COALESCE(c.Nickname, '') AS Nickname    
+        FROM dp_Users u    
+             LEFT OUTER JOIN Contacts c ON c.User_Account = u.User_ID    
+        WHERE User_Name LIKE @Email    
+              OR c.Email_Address LIKE @Email;    
+    ELSE    
+        SELECT u.[User_ID]    
+             , u.[User_Name]    
+             , COALESCE(NULLIF(LTRIM(RTRIM(u.[Display_Name])), ''), NULLIF(LTRIM(RTRIM(CONCAT(COALESCE(c.First_Name, ''), ' ', COALESCE(c.Last_Name, '')))), ''), NULLIF(LTRIM(RTRIM(c.Nickname)), ''), u.[User_Name], '') AS Display_Name    
+             , u.[Password]    
+             , u.[Admin]    
+             , u.[Domain_ID]    
+             , u.[Publications_Manager]    
+             , u.[Contact_ID]    
+             , u.[Supervisor]    
+             , u.[User_GUID]    
+             , u.[Can_Impersonate]    
+             , u.[__ShelbyID]    
+             , u.[__Old_User_ID]    
+             , u.[In_Recovery]    
+             , u.[Data_Service_Permissions]    
+             , u.[Read_Permitted]    
+             , u.[Create_Permitted]    
+             , u.[Update_Permitted]    
+             , u.[Delete_Permitted]    
+             , u.[Time_Zone]    
+             , u.[Locale]    
+             , u.[Theme]    
+             , u.[Setup_Admin]    
+             , u.[Local_Network_Name]    
+             , u.[Login_Attempts]    
+             , u.[MFA_Required]    
+             , u.[Delete_Account_Info]    
+             , u.[Delete_Requested_Date]    
+             , u.[Reason_Deleted]    
+             , u.[__Unmerge_Date]    
+             , COALESCE(c.First_Name, '') AS First_Name    
+             , COALESCE(c.Last_Name, '') AS Last_Name    
+             , COALESCE(c.Nickname, '') AS Nickname    
+        FROM dp_Users u    
+             INNER JOIN Contacts c ON c.Contact_ID = u.Contact_ID    
+        WHERE REPLACE (REPLACE (REPLACE (c.Mobile_Phone, '-', ''), ')', ''), '(', '') LIKE REPLACE (    
+                                                                                               REPLACE (    
+                                                                                                   REPLACE (    
+                                                                                                       @Phone, '-', '')    
+                                                                                                 , ')'    
+                                                                                                 , '')    
+                                                                                             , '('    
+                                                                                             , '');    
+END; 
